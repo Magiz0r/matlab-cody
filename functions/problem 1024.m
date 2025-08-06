6127c52bdaa3002ab8db1cc74bcc7667fa010d0e
Magiz0r <80337841+Magiz0r@users.noreply.github.com>
2025-08-06 22:00:37 +0800

    Create problem 1024.m

---
+++ functions/problem 1024.m
@@ -0,0 +1,6 @@
+Problem 1024. Doubling elements in a vector
+https://www.mathworks.com/matlabcentral/cody/groups/172/problems/1024?title=Doubling+elements+in+a+vector&url=%2Fmatlabcentral%2Fcody%2Fgroups%2F172%2Fproblems%2F1024-doubling-elements-in-a-vector
+
+function B = your_fcn_name(A)
+  B = repelem(A,2);
+end
