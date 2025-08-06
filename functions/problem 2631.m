5cb48fdaba5ce4193f6a11964fa42340ddb05324
Magiz0r <80337841+Magiz0r@users.noreply.github.com>
2025-08-06 21:30:59 +0800

    Create problem 2631.m

---
+++ functions/problem 2631.m
@@ -0,0 +1,6 @@
+Problem 2631. Flip the vector from right to left
+https://www.mathworks.com/matlabcentral/cody/groups/172/problems/2631?title=Flip+the+vector+from+right+to+left&url=%2Fmatlabcentral%2Fcody%2Fgroups%2F172%2Fproblems%2F2631-flip-the-vector-from-right-to-left
+
+function y = flip_vector(x)
+  y = x(end:-1:1);
+end
