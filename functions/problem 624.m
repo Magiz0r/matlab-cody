e1765be54f22d50cb6568813da40ef94e416384c
Magiz0r <80337841+Magiz0r@users.noreply.github.com>
2025-08-06 21:16:20 +0800

    Create problem 624.m

---
+++ functions/problem 624.m
@@ -0,0 +1,6 @@
+Problem 624. Get the length of a given vector
+https://www.mathworks.com/matlabcentral/cody/groups/172/problems/624?title=Get+the+length+of+a+given+vector&url=%2Fmatlabcentral%2Fcody%2Fgroups%2F172%2Fproblems%2F624-get-the-length-of-a-given-vector
+
+function y = VectorLength(x)
+  y = length(x);
+end
