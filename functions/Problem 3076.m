bbbdcaff54489ac6a592cce595098ff055cfabf4
Magiz0r <80337841+Magiz0r@users.noreply.github.com>
2025-08-06 21:33:24 +0800

    Create Problem 3076.m

---
+++ functions/Problem 3076.m
@@ -0,0 +1,6 @@
+Problem 3076. Create a vector
+https://www.mathworks.com/matlabcentral/cody/groups/172/problems/3076-create-a-vector/solutions/14726519
+
+function y = zeroToNby2(n)
+  y = [0:2:n];
+end
