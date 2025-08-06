4f5fe449cc903156515d0d2beb5f90cd824c044f
Magiz0r <80337841+Magiz0r@users.noreply.github.com>
2025-08-06 21:12:45 +0800

    Update problem 247.m

--- functions/problem 247.m
+++ functions/problem 247.m
@@ -1,5 +1,5 @@
 Problem 247. Arrange Vector in descending order
-https://github.com/Magiz0r/matlab-cody/new/main/functions
+https://www.mathworks.com/matlabcentral/cody/groups/172/problems/247
 
 function y = desSort(x)
   y = sort(x,"descend");
