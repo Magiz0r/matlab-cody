4ca97f7aa9a42adf267e0c0f12ff8aa5d9ba08f1
Magiz0r <80337841+Magiz0r@users.noreply.github.com>
2025-08-07 22:59:16 +0800

    Create problem 1974.m

---
+++ functions/problem 1974.m
@@ -0,0 +1,6 @@
+Problem 1974. Length of a short side
+https://www.mathworks.com/matlabcentral/cody/groups/7/problems/1974-length-of-a-short-side/solutions/14727574
+
+function a = calculate_short_side(b, c)
+  a = sqrt(c^2 - b^2);
+end
