0623c72fd2835bc652fd18c2b41c6e82e9d5e7ce
Magiz0r <80337841+Magiz0r@users.noreply.github.com>
2025-08-10 23:52:10 +0800

    Create problem 2017.m

---
+++ functions/problem 2017.m
@@ -0,0 +1,7 @@
+Problem 2017. Side of an equilateral triangle
+https://www.mathworks.com/matlabcentral/cody/groups/7/problems/2017-side-of-an-equilateral-triangle/solutions/14729782
+
+
+function x = side_length(A)
+  x = sqrt(((4*sqrt(3))/3)*A);
+end
