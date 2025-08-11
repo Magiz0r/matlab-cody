eb806a850e9ebef9f8191573cdcb33e01223ae89
Magiz0r <80337841+Magiz0r@users.noreply.github.com>
2025-08-11 23:45:31 +0800

    Create problem 1702.m

---
+++ functions/problem 1702.m
@@ -0,0 +1,6 @@
+Problem 1702. Maximum value in a matrix
+https://www.mathworks.com/matlabcentral/cody/groups/78/problems/1702-maximum-value-in-a-matrix/solutions/14730605
+
+function y = your_fcn_name(x)
+  y = max(x,[],"all");
+end
