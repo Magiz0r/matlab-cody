f56f5c255cd160d525a428216a397b6114ffcd4b
Magiz0r <80337841+Magiz0r@users.noreply.github.com>
2025-08-11 23:27:37 +0800

    Create problem 2016.m

---
+++ functions/problem 2016.m
@@ -0,0 +1,6 @@
+Problem 2016. Area of an equilateral triangle
+https://www.mathworks.com/matlabcentral/cody/groups/7/problems/2016-area-of-an-equilateral-triangle/solutions/14730564
+
+function y = equilateral_area(x)
+  y = sqrt((3*x^2)/4)*x*1/2; 
+end
