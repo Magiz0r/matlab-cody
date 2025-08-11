6c2e2b09989e3923b26f14279b2788ee887eca90
Magiz0r <80337841+Magiz0r@users.noreply.github.com>
2025-08-11 23:29:10 +0800

    Create problem 2015.m

---
+++ functions/problem 2015.m
@@ -0,0 +1,6 @@
+Problem 2015. Length of the hypotenuse
+https://www.mathworks.com/matlabcentral/cody/groups/7/problems/2015-length-of-the-hypotenuse/solutions/14730569
+
+function c = hypotenuse(a,b)
+  c = sqrt(a^2 + b^2);
+end
