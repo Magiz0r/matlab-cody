5f9234416f27b7717d652accb8ab58b0eb6b6c0f
Magiz0r <80337841+Magiz0r@users.noreply.github.com>
2025-08-11 23:47:24 +0800

    Create problem 1545.m

---
+++ functions/problem 1545.m
@@ -0,0 +1,6 @@
+Problem 1545. Return area of square
+https://www.mathworks.com/matlabcentral/cody/groups/78/problems/1545-return-area-of-square/solutions/14730608
+
+function b = area_square(a)
+  b = a^2;
+end
