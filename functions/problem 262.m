aadc67f62cdadc54dceff123415222ad056a725a
Magiz0r <80337841+Magiz0r@users.noreply.github.com>
2025-08-16 23:37:42 +0800

    Create problem 262.m

---
+++ functions/problem 262.m
@@ -0,0 +1,11 @@
+Problem 262. Swap the input arguments
+https://www.mathworks.com/matlabcentral/cody/groups/78/problems/262-swap-the-input-arguments/solutions/14738713
+
+function [q,r] = swapInputs(a,b)
+
+    q = b
+    r = a
+
+
+
+end
