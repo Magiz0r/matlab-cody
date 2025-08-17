d779c32cb6dcc2a4a325add4998e2a32f6b08074
Magiz0r <80337841+Magiz0r@users.noreply.github.com>
2025-08-17 23:01:58 +0800

    Create Problem 838. Check if number exists in vector.m

---
+++ functions/Problem 838. Check if number exists in vector.m
@@ -0,0 +1,6 @@
+Problem 838. Check if number exists in vector
+https://www.mathworks.com/matlabcentral/cody/groups/78/problems/838-check-if-number-exists-in-vector/solutions/14739074
+
+function y = existsInVector(a,b)
+  y = ismember(a,b)
+end
