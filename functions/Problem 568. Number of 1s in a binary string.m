186674a9934c049798847f7830f590420c5756cb
Magiz0r <80337841+Magiz0r@users.noreply.github.com>
2025-08-20 23:46:58 +0800

    Create Problem 568. Number of 1s in a binary string.m

---
+++ functions/Problem 568. Number of 1s in a binary string.m
@@ -0,0 +1,6 @@
+Problem 568. Number of 1s in a binary string
+https://www.mathworks.com/matlabcentral/cody/groups/78/problems/568-number-of-1s-in-a-binary-string/solutions/14741065
+
+function y = one(x)
+  y = count(x,'1');
+end
