dc420b75918f713af78e1ed63b0652a90a1058d8
Magiz0r <80337841+Magiz0r@users.noreply.github.com>
2025-08-21 23:39:54 +0800

    Create Problem 189. Sum all integers from 1 to 2^n.m

---
+++ functions/Problem 189. Sum all integers from 1 to 2^n.m
@@ -0,0 +1,6 @@
+Problem 189. Sum all integers from 1 to 2^n
+https://www.mathworks.com/matlabcentral/cody/groups/78/problems/189-sum-all-integers-from-1-to-2-n
+
+function y = sum_int(x)
+  y = sum(1:2^x)
+end
