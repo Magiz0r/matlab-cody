b0b8100b518506de6f071e6e69b647d58afda686
Magiz0r <80337841+Magiz0r@users.noreply.github.com>
2025-08-23 23:40:06 +0800

    Create Problem 49077. Velocity Conversion.m

---
+++ functions/Problem 49077. Velocity Conversion.m
@@ -0,0 +1,6 @@
+Problem 49077. Velocity Conversion
+https://www.mathworks.com/matlabcentral/cody/groups/52300/problems/49077-velocity-conversion
+
+function y = convert_stuff(x)
+  y = round(x*1.60934,4);
+end
