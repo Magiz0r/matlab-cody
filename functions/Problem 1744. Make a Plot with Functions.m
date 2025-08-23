afe00ae782a3efc3ff4a0afe13aa5d7eb0be618e
Magiz0r <80337841+Magiz0r@users.noreply.github.com>
2025-08-23 23:45:38 +0800

    Create Problem 1744. Make a Plot with Functions.m

---
+++ functions/Problem 1744. Make a Plot with Functions.m
@@ -0,0 +1,13 @@
+Problem 1744. Make a Plot with Functions
+https://www.mathworks.com/matlabcentral/cody/groups/54040/problems/1744
+
+function h = makeAPlot(x,y,color)
+ %%Make a plot with xData (x) yData (y) and color (color) and return the plot as h
+ %% such that "h=plot()"
+ 
+ h = plot(x,y,color);
+
+
+
+ %%end
+end
