260819e35e7b36af6a0f6fd62423735cb8ff9925
Magiz0r <80337841+Magiz0r@users.noreply.github.com>
2025-08-23 23:42:04 +0800

    Create Problem 48905. Potential energy calculationProblem 48905. Potential energy calculation.m

---
+++ functions/Problem 48905. Potential energy calculationProblem 48905. Potential energy calculation.m
@@ -0,0 +1,6 @@
+Problem 48905. Potential energy calculation
+https://www.mathworks.com/matlabcentral/cody/groups/52300/problems/48905-potential-energy-calculation
+
+function PE = your_fcn_name(m,h)
+  PE = m*10*h;
+end
