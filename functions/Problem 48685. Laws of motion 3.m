b827585adcd25f136d7abeb0528038d0e88edcc1
Magiz0r <80337841+Magiz0r@users.noreply.github.com>
2025-08-23 23:33:56 +0800

    Create Problem 48685. Laws of motion 3.m

---
+++ functions/Problem 48685. Laws of motion 3.m
@@ -0,0 +1,6 @@
+Problem 48685. Laws of motion 3
+https://www.mathworks.com/matlabcentral/cody/groups/52300/problems/48685-laws-of-motion-3
+
+function v = your_fcn_name(u,a,s)
+  v = round(sqrt(u^2 + 2*a*s));
+end
