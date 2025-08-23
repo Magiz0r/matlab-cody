d3085f42f366c22ef758ccfebf407d351f6b51dc
Magiz0r <80337841+Magiz0r@users.noreply.github.com>
2025-08-23 23:36:09 +0800

    Create Problem 55390. Force and Motion 3.m

---
+++ functions/Problem 55390. Force and Motion 3.m
@@ -0,0 +1,7 @@
+Problem 55390. Force and Motion 3
+https://www.mathworks.com/matlabcentral/cody/groups/52300/problems/55390-force-and-motion-3
+
+function N = netforce(F1, F2) % Do not edit this line.
+  % insert your code here
+  N = F1 + F2;
+end % Do not edit this line.
